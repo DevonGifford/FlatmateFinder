ed33767a48ae5219c8441437e2a9b2ac4c8a3f21
DevonGifford <DevonGifford@outlook.com>
2023-11-21 20:00:00 -0600

    design: created an accordian UI layout for faq page

--- src/pages/Faq.page.tsx
+++ src/pages/Faq.page.tsx
@@ -1,14 +1,46 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function FaqPage() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <div className="text-4xl font-semibold italic mt-8">Welcome to the</div>
-        <div className="text-2xl italic mt-4 mb-8">
-          Frequently Asked Questions 
+      <div className="flex flex-col justify-center items-center sm:mx-20 md:max-w-10/12 sm:max-w-4/6 gap-5 md:gap-8">
+        <div className="text-4xl italic py-4 pb-6">
+          Frequently Asked Questions
         </div>
+        <Accordion type="single" collapsible className="w-full text-center">
+          <AccordionItem className=" border-none" value="item-1">
+            <AccordionTrigger className="flex-col justify-center gap-1 text-xl">
+              When is the room available
+            </AccordionTrigger>
+            <AccordionContent className="mx-8">
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem className=" border-none" value="item-2">
+            <AccordionTrigger className="flex-col justify-center gap-1 text-xl">
+              How close is the nearest gym, shop, metro?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other
+              components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem className=" border-none" value="item-3">
+            <AccordionTrigger className="flex-col justify-center gap-1 text-xl">
+              How many people live in the apartment?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It&apos;s animated by default, but you can disable it if you
+              prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
-      <div className="flex flex-col gap-3"></div>
     </>
   );
 }
