d5a474d788b46ce9fe8be22c6f0a1700461799ff
DevonGifford <DevonGifford@outlook.com>
2023-11-21 20:00:00 -0600

    init: creating new branch and skeletoning new page

---
+++ src/pages/Faq.page.tsx
@@ -0,0 +1,14 @@
+
+export default function FaqPage() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="text-4xl font-semibold italic mt-8">Welcome to the</div>
+        <div className="text-2xl italic mt-4 mb-8">
+          Frequently Asked Questions 
+        </div>
+      </div>
+      <div className="flex flex-col gap-3"></div>
+    </>
+  );
+}
