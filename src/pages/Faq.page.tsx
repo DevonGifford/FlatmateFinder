5a3e277f1a1e198a6dbd92f0c99a2dc0f4cb103e
DevonGifford <DevonGifford@outlook.com>
2023-11-22 20:00:00 -0600

    feat: include FAQ JSON files for (_en) and (_es) versions

--- src/pages/Faq.page.tsx
+++ src/pages/Faq.page.tsx
@@ -5,40 +5,29 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+import faqData from "../lib/constants/faq_en.json";
+
+
 export default function FaqPage() {
   return (
     <>
       <div className="flex flex-col justify-center items-center sm:mx-20 md:max-w-10/12 sm:max-w-4/6 gap-5 md:gap-8">
-        <div className="text-4xl italic py-4 pb-6">
+        <div className="text-4xl italic py-4 pb-6 border-b-2">
           Frequently Asked Questions
         </div>
         <Accordion type="single" collapsible className="w-full text-center">
-          <AccordionItem className=" border-none" value="item-1">
-            <AccordionTrigger className="flex-col justify-center gap-1 text-xl">
-              When is the room available
-            </AccordionTrigger>
-            <AccordionContent className="mx-8">
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem className=" border-none" value="item-2">
-            <AccordionTrigger className="flex-col justify-center gap-1 text-xl">
-              How close is the nearest gym, shop, metro?
-            </AccordionTrigger>
-            <AccordionContent>
-              Yes. It comes with default styles that matches the other
-              components&apos; aesthetic.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem className=" border-none" value="item-3">
-            <AccordionTrigger className="flex-col justify-center gap-1 text-xl">
-              How many people live in the apartment?
-            </AccordionTrigger>
-            <AccordionContent>
-              Yes. It&apos;s animated by default, but you can disable it if you
-              prefer.
-            </AccordionContent>
-          </AccordionItem>
+
+        {faqData.faq_questions.map((faq, index) => (
+            <AccordionItem key={`faq-${index}`} className="border-none py-2" value={`item-${index}`}>
+              <AccordionTrigger className="flex-col justify-center gap-1 py-2 text-lg">
+                {faq.question}
+              </AccordionTrigger>
+              <AccordionContent className="mx-8">
+                {faq.answer}{" "}{faq.emoji}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+         
         </Accordion>
       </div>
     </>
