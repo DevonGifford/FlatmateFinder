e665a50a8f372743490e6c65c95ae65fd0da3948
DevonGifford <DevonGifford@outlook.com>
2023-11-22 20:00:00 -0600

    wip: prototyping useLanguageContext with faq question data

--- src/pages/Faq.page.tsx
+++ src/pages/Faq.page.tsx
@@ -5,10 +5,20 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
-import faqData from "../lib/constants/faq_en.json";
-
+import faqData_EN from "../lib/constants/faq_en.json";
+import faqData_ES from "../lib/constants/faq_es.json";
+import { useLanguageContext } from "@/components/contexts/language/useLanguageContext";
 
 export default function FaqPage() {
+  const { language } = useLanguageContext(); // Access language from the context
+
+  let questionData;
+  if (language === "english") {
+    questionData = faqData_EN;
+  } else {
+    questionData = faqData_ES;
+  }
+
   return (
     <>
       <div className="flex flex-col justify-center items-center sm:mx-20 md:max-w-10/12 sm:max-w-4/6 gap-5 md:gap-8">
@@ -16,18 +26,20 @@ export default function FaqPage() {
           Frequently Asked Questions
         </div>
         <Accordion type="single" collapsible className="w-full text-center">
-
-        {faqData.faq_questions.map((faq, index) => (
-            <AccordionItem key={`faq-${index}`} className="border-none py-2" value={`item-${index}`}>
+          {questionData.faq_questions.map((faq, index) => (
+            <AccordionItem
+              key={`faq-${index}`}
+              className="border-none py-2"
+              value={`item-${index}`}
+            >
               <AccordionTrigger className="flex-col justify-center gap-1 py-2 text-lg">
                 {faq.question}
               </AccordionTrigger>
               <AccordionContent className="mx-8">
-                {faq.answer}{" "}{faq.emoji}
+                {faq.answer} {faq.emoji}
               </AccordionContent>
             </AccordionItem>
           ))}
-         
         </Accordion>
       </div>
     </>
